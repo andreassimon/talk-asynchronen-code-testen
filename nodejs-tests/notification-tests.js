aa058b123454b919e399c6bae7da48c548dcb7a2
Andreas Simon <a.simon@quagilis.de>
2014-10-15 12:34:57 +0200

    Merged messaging code into Mocha test

--- nodejs-tests/notification-tests.js
+++ nodejs-tests/notification-tests.js
@@ -2,57 +2,35 @@ var amqp = require('amqp');
 
 var connection = amqp.createConnection({ host: 'localhost' });
 
-// Wait for connection to become established.
-connection.on('ready', function () {
-  console.log('AMQP connection is ready');
+var FIB_MIN = 832040;
 
-  // Use the default 'amq.topic' exchange
-  connection.queue('my-queue', function(q) {
-      // Catch all messages
-      q.bind('#');
+describe('AMQP Fibonacci service', function() {
+  it('calculates fib(30)', function(done) {
+    // Wait for connection to become established.
+    connection.on('ready', function () {
+      console.log('AMQP connection is ready');
 
-      // Receive messages
-      q.subscribe(function (message) {
-        // Print messages to stdout
-        // require('inspect')(message.data);
-        console.log(message.data.toString());
-        console.log(message);
+      // Use the default 'amq.topic' exchange
+      connection.queue('my-queue', function(q) {
+          // Catch all messages
+          q.bind('#');
+
+          // Receive messages
+          q.subscribe(function (message) {
+            // Print messages to stdout
+            // require('inspect')(message.data);
+            console.log(message.data.toString());
+            console.log(message);
+            // console.log(assertEquals);
+            // assertEquals(FIB_MIN, message.data.toString());
+            done();
+          });
+
+        connection.publish('calculate-fibonacci', '30', { replyTo: 'my-queue'}, function() {
+          console.log('Message published');
+        });
+
       });
-
-    connection.publish('calculate-fibonacci', '30', { replyTo: 'my-queue'}, function() {
-      console.log('Message published');
     });
-
   });
 });
-
-// var options = {
-//   hostname: 'localhost',
-//   port: 3000,
-//   path: '/',
-//   method: 'POST'
-// };
-
-// var req = http.request(options, function(res) {
-//   console.log('STATUS: ' + res.statusCode);
-//   console.log('HEADERS: ' + JSON.stringify(res.headers));
-//   res.setEncoding('utf8');
-//   res.on('data', function (chunk) {
-//     console.log('BODY: ' + chunk);
-//   });
-// });
-
-// req.on('error', function(e) {
-//   console.log('problem with request: ' + e.message);
-// });
-
-// // write data to request body
-// req.write('data\n');
-// req.write('data\n');
-// req.end();
-
-// describe('AMQP Fibonacci service', function() {
-//   it('calculates fib(30)', function() {
-     
-//   });
-// });
